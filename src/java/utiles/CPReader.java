e7b511beab878da62fb7d4ef67897e62d0eaeafe
Myrna <myrna.oralia@gmail.com>
2018-04-27 17:57:56 -0700

    CPReader
    Se corrigen errores y se hacen pruebas de funcionalidad

--- src/java/utiles/CPReader.java
+++ src/java/utiles/CPReader.java
@@ -8,8 +8,10 @@ package utiles;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -20,28 +22,31 @@ public class CPReader {
     public final String cpBD = "C:\\Users\\UABC-MARY\\Documents\\NetBeansProjects\\SisAECH\\src\\java\\utiles\\CPdescarga.txt";
     //private int codigoPostal = -1;
     private List<String> colonia;
-    private List<String> estado;
-    private List<String> municipio;
+    private String estado;
+    private String municipio;
     private String linea = "";
-    private List<Object> cPs;
+    
 
     
     
     public List<Object> getDomicilio(int codigoPostal){
         //Iniciar flujo de lectura de archivo CSV
+        List<Object> cPs = new ArrayList<>();
+        colonia = new ArrayList<>();
         try{
             BufferedReader bsr = new BufferedReader(new FileReader(cpBD));
-            cPs = null;
+            //List<Object> cPs = new ArrayList<>();
             while(linea != null){
                 
                 //Recursion para obtener todos los valores repetidos
                 //Verificar acentos para que se muestren en consola
                 if(linea.startsWith(Integer.toString(codigoPostal)) == true){
-                    System.out.println(linea);
-                    String splitLinea[] = linea.split("|");
+                    
+                    String splitLinea[] = linea.split(Pattern.quote("|"));
+                    
                     colonia.add(splitLinea[1]);
-                    municipio.add(splitLinea[2]);//No se necesita llenar arreglo ya que todos los renglones del mismo CP va a ser igual
-                    estado.add(splitLinea[3]);
+                    municipio = splitLinea[3];//No se necesita llenar arreglo ya que todos los renglones del mismo CP va a ser igual
+                    estado = splitLinea[4];
                 }
                 else{}
                 linea = bsr.readLine();
@@ -50,7 +55,7 @@ public class CPReader {
             cPs.add(municipio);
             cPs.add(estado);
         }catch(IOException ex){
-            System.out.println("Error en CPREader: "+ex.getMessage());
+            System.out.println("Error en CPREader: " + ex.getMessage());
             System.out.println(Arrays.toString(ex.getStackTrace()));
         }
         
@@ -59,6 +64,17 @@ public class CPReader {
     
     public static void main(String[] args){
         CPReader reader = new CPReader();
-        reader.getDomicilio(21396);
+        List<Object> obs = reader.getDomicilio(21396);
+        List<String> col = (List<String>) obs.get(0);
+        String mun = (String) obs.get(1);
+        String est = (String) obs.get(2);
+        
+        System.out.println("Municipio: " + mun);
+        System.out.println("Estado: " + est);
+        System.out.println("Colonia: ");
+        col.forEach((col1) -> {
+            System.out.println(col1);
+        });
+            
     }
 }
